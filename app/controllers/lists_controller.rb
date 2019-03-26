6e71204a1a4961203f5ae8d9e07f4903a4b5d30e
Nicolas Delage <ndelage90@gmail.com>
2019-03-26 12:32:28 -0600

    add feature to create simple list without places

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,26 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :edit, :update, :destroy]
   def index
+    @lists = List.all
   end
 
   def show
+
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    #authorize @list
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def update
@@ -18,5 +30,16 @@ class ListsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def find_list
+    @list = List.find(params[:id])
+    #authorize @list
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :description, :is_public)
   end
 end
