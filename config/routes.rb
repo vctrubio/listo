1fde2f8e8404e1c82a665372b328583d35591f58
Amber Lowey <lowey45@gmail.com>
2019-03-27 13:22:02 -0600

    basic follower functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'lists#index'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :lists
   resources :places, only: [:show, :update, :edit, :destroy]
   resources :users, only: [:show, :new, :create, :update, :edit, :destroy]
   resources :listplaces, only: [:new, :create, :destroy]
   resources :favourites, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :relationships, only: [:create, :destroy]
 end
