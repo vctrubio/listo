fe4bdf783612bed7d0626d3038d3b124238f3e7e
Nicolas Delage <ndelage90@gmail.com>
2019-03-28 19:52:20 -0600

    fill name and complete address

--- app/javascript/packs/form/autocomplete.js
+++ app/javascript/packs/form/autocomplete.js
@@ -9,27 +9,29 @@ const autocomplete = () => {
   // Get the place details from the autocomplete object.
   var place = autocomplete.getPlace();
   console.log(place);
+  var full_address = "";
   for (var i in place.address_components) {
     var component = place.address_components[i];
 
-    for (var j in component.types) {
-      // Some types are ["country", "political"]
-      var type_element = document.getElementById(component.types[j]);
+    full_address += component.long_name + " ";
 
-      if (type_element) {
-        type_element.value = component.long_name;
-      }
-    }
+
+    // for (var j in component.types) {
+    //   // Some types are ["country", "political"]
+    //   var type_element = document.getElementById(component.types[j]);
+
+    //   if (type_element) {
+    //     type_element.value = component.long_name;
+    //   }
+    // }
+
   }
+  console.log(full_address);
+  document.getElementById('full_address').value = full_address;
+  document.getElementById('autocomplete').value = place.name;
 }
 }
 
 export default autocomplete;
 
-  // const trigger = document.getElementById('insertForm');
-  // const forms = trigger.dataset.forms.split('$$$$$');
-  // let counter = 0;
-  // trigger.addEventListener('click', event => {
-  //   event.target.insertAdjacentHTML("beforebegin", forms[counter]);
-  //   counter++;
-  // })
+
