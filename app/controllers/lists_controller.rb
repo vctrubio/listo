e3ddc12581323eb6269ea620994f35e0fe159f48
Amber Lowey <lowey45@gmail.com>
2019-04-01 13:07:03 -0600

    delete button on list show page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -35,12 +35,15 @@ class ListsController < ApplicationController
   end
 
   def update
+    puts "THIS WAS A PATCH REQUEST"
   end
 
   def edit
   end
 
   def destroy
+    @list.destroy
+    redirect_to lists_path
   end
 
   private
