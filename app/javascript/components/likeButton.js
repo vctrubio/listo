3625318d1743a04c0ec2795c97f6132a5747a9fc
Derek Saffe <desaff@gmail.com>
2019-04-03 14:00:45 -0600

    got the like ajax to finally work and added pulse effect

--- app/javascript/components/likeButton.js
+++ app/javascript/components/likeButton.js
@@ -1,7 +1,14 @@
 const likeButton = document.querySelector('#like-button');
+const dislikeButton = document.querySelector('#dislike-button');
 
 likeButton.addEventListener('click', (event) => {
-  console.log(event);
+  // likeButton.className.toggle("hide");
+  console.log('DisLike')
 });
 
-export { likeButton };
+dislikeButton.addEventListener('click', (event) => {
+  // dislikeButton.className.toggle("hide");
+  console.log('Like')
+});
+
+export { likeButton, dislikeButton };
