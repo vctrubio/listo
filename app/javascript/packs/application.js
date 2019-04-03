156e38ca7fb306096039d71456c04b922a0338de
Derek Saffe <desaff@gmail.com>
2019-04-03 16:39:06 -0600

    fixed mapbox bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,12 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import "bootstrap";
 //import insert from './form/insertForm';
 //import autocomplete from './form/autocomplete';
+import { initMapbox } from './plugins/init_mapbox';
 
 //insert();
 //autocomplete();
 initMapbox();
+
 
 
 // import { initUpdateNavbarOnScroll } from '../components/navbar';
