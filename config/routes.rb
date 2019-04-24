02af32bdce3b730be907e41d0665035729ca5eb3
Nicolas Delage <ndelage90@gmail.com>
2019-04-24 18:12:47 -0500

    url for profile looks better with user name, still bug when editing username

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'lists#index'
-  resources :users do
+  resources :users, param: :slug, only: [:show, :new, :create, :update, :edit, :destroy] do
     member do
       get :following, :followers
     end
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :places, only: [:show, :update, :edit, :destroy]
-  resources :users, only: [:show, :new, :create, :update, :edit, :destroy]
+
   #resources :listplaces, only: [:new, :create, :destroy]
   resources :favourites, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
